e7fc91e329d6f9f2b4d879527cfeb7bb318841ef
Franco Teodoro <franchecool1@gmail.com>
2025-10-02 01:55:20 -0300

    updated to final version, without c decryption working

---
+++ cliente_aes_final.py
@@ -0,0 +1,170 @@
+#cliente_aes_final.py
+import socket
+import numpy as np
+import cv2
+import time
+import ctypes
+from ctypes import *
+from PIL import Image
+import aes_lib
+import statistics
+from statistics import mean
+import matplotlib.pyplot as plt
+from numpy import asarray
+aes_c = CDLL("C:\\Users\\franc\\Downloads\\AES_TA\\aes_lib.so")
+"CLiente encripta la imagen y envía la imagen encriptada al servidor"
+aes_c.make_key()
+
+def filtro_mediana(sign, ventana):
+    signc = []
+    tam = len(sign)
+    offs = int((ventana-1)/2)
+    for i in range(tam):
+        inicio = i - offs if (i - offs > 0) else i
+        fin = i + offs if (i + offs < tam) else tam
+        signc.append(statistics.median(sign[inicio:fin]))
+    return signc
+
+
+
+# llave de 16 bits
+master_key = 0x6672616e636f67656e61726f78617669
+
+llave = aes_lib.make_key(master_key)   
+
+if __name__ == "__main__":
+    
+    llave = aes_lib.make_key(master_key)
+        
+    tot_encrypted = []
+    tot_encrypted_c = []
+    encrypted_to_bytes = []	
+    encrypted_to_bytes_c = []	
+    header_to_bytes = []
+    
+    time_encrypt_py = []
+    time_encrypt_c = []
+    time_py_median = []
+    time_c_median = []
+    HOST = "127.0.0.1"
+    PORT = 5005
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST,PORT))        
+        im = open("tux.bmp","rb")
+        l = im.read()
+        lenght_image = len(l)
+        print(f"longitud de la imagen {lenght_image}")
+        longitud_encode = str(lenght_image).encode("utf-8")
+        "enviamos longitud de la imagen sin particion"
+        s.sendall(longitud_encode) #0       
+ 
+        # "Convertimos la imagen de formato byte a hexadecimal"        
+        # hexadecimal_string = l.hex()
+        # "Convertimos de formato hexadecimal a integer"
+        # str_to_int = int(hexadecimal_string, 16)        
+        "Separamos la data de la imagen en Bits de Headers y Bits de data(píxeles)"
+        
+        "Bits de headers"
+        header_bits = l[:54]
+        "Bits de data(píxeles)"
+        image_bits = l[54:]
+        
+        
+        
+
+        
+        "Convertimos los píxeles a a integer"
+        image_int = int.from_bytes(image_bits, "big")  
+        "El formato AES encripta en bloques de 128 bits"
+        data_split =aes_lib.split_bits(image_int,128)
+        len_data_split = str(len(data_split)).encode("utf-8")
+        "Envíamos la cantidad de bloques de 128 bits que se encriptarán de la imagen"            
+        s.sendall(len_data_split) #1
+        "Tenemos un arreglo de int de datos encriptados"
+        
+
+        for i in range(len(data_split)):
+            
+            data_split_8= aes_lib.split_bits(data_split[i],8)
+            vectorx = np.asarray(data_split_8).astype('int32')
+            arr = (ctypes.c_ubyte * len(vectorx))(*vectorx)
+            start1 = time.time()
+            res1 = aes_c.encrypt(arr)
+            end1 = time.time()
+            tiempo1 = end1 - start1
+            time_encrypt_c.append(tiempo1)
+            if res1 == 0: 
+                encrypt_c = ','.join(map(str, arr))
+            encrypt_c =aes_lib.str_to_int(encrypt_c)
+            start2 = time.time()
+            encrypted = aes_lib.encrypt(data_split[i],llave)
+            end2 = time.time()
+            tiempo2 = end2 - start2
+            time_encrypt_py.append(tiempo2)
+            tot_encrypted.append(encrypted)
+            tot_encrypted_c.append(encrypt_c)    
+
+        " arreglo de bytes de datos encriptados"
+        for i in range(len(tot_encrypted)):
+            encrypted_to_bytes.append(tot_encrypted[i].to_bytes(16, 'big')) 
+            encrypted_to_bytes_c.append(tot_encrypted_c[i].to_bytes(16, 'big'))
+        image_encrypted_bits = (b''.join(encrypted_to_bytes))
+        image_encrypted_bits_c = (b''.join(encrypted_to_bytes_c))
+        "enviamos datos encriptados en python" 
+        inicio= time.perf_counter()
+        s.sendall(image_encrypted_bits) #2
+        fin=time.perf_counter()
+        
+        "enviamos datos encriptados en c"
+        # s.sendall(image_encrypted_bits_c) #3
+        # print(f"Tiempo para enviar datos encriptados: \n{fin-inicio}")
+
+        "enviamos la informacion del header"
+
+        
+        s.sendall(header_bits) #4
+       
+        index = range(len(data_split))
+        "join header con datos encriptados"
+        image_total_encrypted = b''.join([header_bits,image_encrypted_bits])
+        bytes_encripted = bytearray(image_total_encrypted)
+        
+        image_total_encrypted_c = b''.join([header_bits,image_encrypted_bits_c])
+        bytes_encripted_c = bytearray(image_total_encrypted_c)
+        print(f"longitud de bytes en Python: {len(bytes_encripted)}")
+        print(f"longitud de bytes en C : {len(bytes_encripted_c)}")
+
+           
+        "imprimimos la imagen encriptada"
+        inicio1=time.perf_counter()
+        numpyarray = np.asarray(bytes_encripted, dtype=np.ubyte)
+        numpyarray_c = np.asarray(bytes_encripted_c, dtype=np.ubyte)
+        print(f"longitud de numpyarray {len(numpyarray)}")
+        print(f"longitud de numpyarray_c {len(numpyarray_c)}")
+        bgrImage = cv2.imdecode(numpyarray, cv2.IMREAD_COLOR)
+        bgrImage_c = cv2.imdecode(numpyarray_c, cv2.IMREAD_COLOR)
+        im = Image.fromarray(bgrImage)
+        im_c = Image.fromarray(bgrImage_c)
+        im.save("Imagen_encriptada_cliente_py.bmp")
+        im.save("Imagen_encriptada_cliente_c.bmp")       
+        fin1=time.perf_counter()
+        print(f"Tiempo de recibir e imprimir la imagen encriptada: \n{fin1-inicio1}")    
+       
+    ltb2 = (filtro_mediana(time_encrypt_py[:100],4))
+
+    plt.plot(index[:100],ltb2[:100],label = 'tiempos en py')
+    plt.plot(index[:100],time_encrypt_c[:100],label = "tiempos en c")
+    plt.xlabel("ejecuciones")
+    plt.ylabel("tiempos")
+    plt.tight_layout()
+    plt.legend()    
+    plt.savefig("tiempos_encryptacion.png",dpi = 500)
+    plt.close
+    
+
+    plt.plot(mean(time_encrypt_py[100:])/mean(time_encrypt_c[100:]),'-ro',label='speedup')
+    plt.xlabel('N')
+    plt.ylabel('Speedup')
+    plt.legend()
+    plt.savefig("tiempos_promedios de encriptacion3.png",dpi = 500)      
+     
---
+++ servidor_aes_final.py
@@ -0,0 +1,168 @@
+#servidor_aes_final.py
+import socket
+import numpy as np
+import cv2
+import time
+import ctypes
+from ctypes import *
+from PIL import Image
+import aes_lib
+aes_c = CDLL("C:\\Users\\franc\\Downloads\\AES_TA\\aes_lib.so")
+aes_c.make_key()
+from statistics import mean
+import matplotlib.pyplot as plt
+import statistics
+master_key = 0x6672616e636f67656e61726f78617669
+llave = aes_lib.make_key(master_key)
+
+
+tot_decrypted = []
+tot_decrypted_c = []
+decrypted_to_bytes = [] 
+decrypted_to_bytes_c = []
+n = 0
+SOCK_BUFFER = 32768
+
+def filtro_mediana(sign, ventana):
+    signc = []
+    tam = len(sign)
+    offs = int((ventana-1)/2)
+    for i in range(tam):
+        inicio = i - offs if (i - offs > 0) else i
+        fin = i + offs if (i + offs < tam) else tam
+        signc.append(statistics.median(sign[inicio:fin]))
+    return signc
+
+if __name__ == '__main__':
+   
+    HOST = '127.0.0.1'
+    PORT = 5005
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        while n <1:
+            print("Esperando conexiones")
+            conn,addr = s.accept()
+            with conn:
+                "recibe longitud de imagen"
+                print(f'Conectado por{addr}')
+                data0 = conn.recv(SOCK_BUFFER)#0
+                decode_data0 = int(data0.decode("utf-8"))
+                SOCK_BUFFER = decode_data0
+
+                "Recibimos  la cantidad de bloques de 128 bits que se encriptarán de la imagen"
+                data1 = conn.recv(SOCK_BUFFER)#1
+                len_encrypted = data1
+                
+                "recibimos los datos encriptados en python"
+                inicio=time.perf_counter()
+                data2 = conn.recv(SOCK_BUFFER)#2
+                fin=time.perf_counter()
+                print(f"Tiempo para recibir los datos encriptados: \n {fin-inicio}")
+                print("data2",len(data2))
+                
+                # "recibimos los datos encriptados en c"
+                # data3 = conn.recv(SOCK_BUFFER)#3
+                # print("data3",len(data3))
+
+                time_decrypt_py = []
+                time_decrypt_c = []
+
+                
+                "bits to int"
+                "split into 128 bits blocks"
+                image_int = int.from_bytes(data2, "big")  
+                data_split =aes_lib.split_bits(image_int,128)   
+                image_int_c = int.from_bytes(data2, "big") 
+                data_split_c = aes_lib.split_bits(image_int_c,128)          
+                "Desencriptamos los datos recibidos"
+                inicio1=time.perf_counter() 
+                for i in range(len(data_split)):
+                    vectory = np.zeros((16,), dtype=int)
+                    arr1 = (ctypes.c_ubyte * len(vectory))(*vectory)
+                    start1 = time.time()
+                    res2 = aes_c.decrypt(arr1)
+                    end1 = time.time()
+                    tiempo1 = end1-start1
+                    time_decrypt_c.append(tiempo1)
+                    if res2 == 0: 
+                        decrypt_c = ','.join(map(str, arr1))
+
+                    decrypt_c =aes_lib.str_to_int(decrypt_c)              
+
+                    start2 = time.time()
+                    decrypted = aes_lib.decrypt(data_split[i],llave)  
+                    end2 = time.time()
+                    tiempo2 = end2 - start2
+                    time_decrypt_py.append(tiempo2)
+                    tot_decrypted.append((decrypted))          
+                    tot_decrypted_c.append((decrypt_c))      
+                fin1=time.perf_counter()
+                print(f"Tiempo para desencriptar los datos recibidos: \n{fin1-inicio1} "  )
+              
+                " arreglo de bytes de datos desencriptados"
+                for i in range(len(tot_decrypted)):
+                    decrypted_to_bytes.append(tot_decrypted[i].to_bytes(16, 'big')) 
+                    decrypted_to_bytes_c.append(tot_decrypted_c[i].to_bytes(16,'big'))
+                image_decrypted_bits = (b''.join(decrypted_to_bytes))
+                image_decrypted_bits_c = (b''.join(decrypted_to_bytes_c))
+                
+                
+                
+                "recibimos los datos de los headers en bits"
+                
+                data4 = conn.recv(SOCK_BUFFER)#4
+                header_bits = data4 
+                index = range(len(data_split))
+                "join header con datos desencriptados"
+                image_total_decrypted = b''.join([header_bits,image_decrypted_bits])
+                image_total_decrypted_c = b''.join([header_bits,image_decrypted_bits_c])
+                bytes_decrypted = bytearray(image_total_decrypted)                
+                bytes_decrypted_c = bytearray(image_total_decrypted_c)
+                print(f"longitud de bytes: {len(bytes_decrypted)}")
+                print(f"longitud de bytes_c: {len(bytes_decrypted_c)}")
+
+                "imprimimos la imagen desencriptada"
+                inicio2=time.perf_counter()
+                numpyarray = np.asarray(bytes_decrypted, dtype=np.ubyte)
+                numpyarray_c = np.asarray(bytes_decrypted_c, dtype=np.ubyte)
+                print(f"longitud de numpyarray {len(numpyarray)}")
+                print(f"longitud de numpyarray_c {len(numpyarray_c)}")
+
+
+                imageBGR_c = cv2.imdecode(numpyarray_c, cv2.IMREAD_COLOR)
+                if imageBGR_c is None:
+                    print("Error al cargar la imagen en C")
+                imageRGB_c = cv2.cvtColor(imageBGR_c , cv2.COLOR_BGR2RGB)
+
+                imageBGR = cv2.imdecode(numpyarray, cv2.IMREAD_COLOR)
+                if imageBGR is None:
+                    print("Error al cargar la imagen en python")
+                imageRGB = cv2.cvtColor(imageBGR , cv2.COLOR_BGR2RGB)
+
+                im_c = Image.fromarray(imageRGB_c)
+                im = Image.fromarray(imageRGB)
+
+
+                im.save("Imagen_desencriptada_servidor_tux_py.bmp")  
+                im_c.save("Imagen_desencriptada_servidor_tux_c.bmp")
+                fin2=time.perf_counter()    
+                print(f"Tiempo para imprimir la imagen desencriptada: \n{fin2-inicio2}")         
+                ltb2 = (filtro_mediana(time_decrypt_py[:100],3))
+
+                plt.plot(index[:100],ltb2[:100],label = 'tiempos en py')
+                plt.plot(index[:100],time_decrypt_c[:100],label = "tiempos en c")
+                plt.xlabel("ejecuciones")
+                plt.ylabel("tiempos")
+                plt.tight_layout()
+                plt.legend()    
+                plt.savefig("tiempos_desencriptación.png",dpi = 500)
+                plt.close
+
+
+                plt.plot(mean(time_decrypt_py[100:])/mean(time_decrypt_c[100:]),'-ro',label='speedup')
+                plt.xlabel('N')
+                plt.ylabel('Speedup')
+                plt.legend()
+                plt.savefig("tiempos_promedios de desencriptacion3.png",dpi = 500)  
+            n+=1                              
